31b356e8998d064cf3a4885ca35d08aa83e8f6c2
Ubong Ndoh <ubongndoh@gmail.com>
2022-02-04 12:00:44 +0100

    navbar completed

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,16 @@ import HelloWorld from './components/HelloWorld.vue';
 
 <template>
 	<header
-		class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2"
+		class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2 mt-4"
 	>
 		<div class="flex-1 flex justify-between items-center">
 			<a href="#">
-				<svg
-					width="32"
-					height="36"
-					viewBox="0 0 32 36"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M15.922 35.798c-.946 0-1.852-.228-2.549-.638l-10.825-6.379c-1.428-.843-2.549-2.82-2.549-4.501v-12.762c0-1.681 1.12-3.663 2.549-4.501l10.825-6.379c.696-.41 1.602-.638 2.549-.638.946 0 1.852.228 2.549.638l10.825 6.379c1.428.843 2.549 2.82 2.549 4.501v12.762c0 1.681-1.12 3.663-2.549 4.501l-10.825 6.379c-.696.41-1.602.638-2.549.638zm0-33.474c-.545 0-1.058.118-1.406.323l-10.825 6.383c-.737.433-1.406 1.617-1.406 2.488v12.762c0 .866.67 2.05 1.406 2.488l10.825 6.379c.348.205.862.323 1.406.323.545 0 1.058-.118 1.406-.323l10.825-6.383c.737-.433 1.406-1.617 1.406-2.488v-12.757c0-.866-.67-2.05-1.406-2.488l-10.825-6.379c-.348-.21-.862-.328-1.406-.328zM26.024 13.104l-7.205 13.258-3.053-5.777-3.071 5.777-7.187-13.258h4.343l2.803 5.189 3.107-5.832 3.089 5.832 2.821-5.189h4.352z"
-					></path>
-				</svg>
+				<div class="flex flex-row justify-between items-center">
+					<img src="./assets/A U.svg" alt="" />
+					<span class="uppercase mx-4 font-bold text-2xl title1"
+						>awesome ui</span
+					>
+				</div>
 			</a>
 		</div>
 
@@ -43,44 +39,40 @@ import HelloWorld from './components/HelloWorld.vue';
 				>
 					<li
 						><a
-							class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
+							class="lg:p-4 py-3 px-0 block border-b-2 border-transparent lg:hover:border-emerald-600 uppercase"
 							href="#"
-							>Features</a
+							>About Us</a
 						></li
 					>
 					<li
 						><a
-							class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
+							class="lg:p-4 py-3 px-0 block border-b-2 border-transparent lg:hover:border-emerald-600 uppercase"
 							href="#"
-							>Pricing</a
+							>Contact Us</a
 						></li
 					>
 					<li
 						><a
-							class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
+							class="lg:p-4 py-3 px-0 block border-b-2 border-transparent lg:hover:border-emerald-600 uppercase"
 							href="#"
-							>Documentation</a
+							>Store</a
 						></li
 					>
 					<li
 						><a
-							class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2"
+							class="lg:p-4 py-3 px-0 block border-b-2 border-transparent lg:hover:border-emerald-600 lg:mb-0 mb-2 uppercase"
 							href="#"
-							>Support</a
+							>Use Cases</a
+						></li
+					><li
+						><a
+							class="lg:py-2 py-3 px-7 block border-b-2 border-transparent bg-emerald-600 lg:mb-0 mb-2 text-white font-extrabold lg:ml-6 uppercase rounded"
+							href="#"
+							>Register</a
 						></li
 					>
 				</ul>
 			</nav>
-			<a
-				href="#"
-				class="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor"
-			>
-				<img
-					class="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400"
-					src="https://pbs.twimg.com/profile_images/1128143121475342337/e8tkhRaz_normal.jpg"
-					alt="Andy Leverenz"
-				/>
-			</a>
 		</div>
 	</header>
 </template>
@@ -95,6 +87,9 @@ import HelloWorld from './components/HelloWorld.vue';
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
+	/* margin-top: 60px; */
+}
+.title1 {
+	color: #647b64;
 }
 </style>
